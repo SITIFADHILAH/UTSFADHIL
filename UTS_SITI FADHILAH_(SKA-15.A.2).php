eb9c2bcd1a05eff81bf1057363f2f46965a6b2c3
SITIFADHILAH <dedhexs@yahoo.com>
2017-05-29 13:55:39 +0700

    Add files via upload

---
+++ UTS_SITI FADHILAH_(SKA-15.A.2).php
@@ -0,0 +1,30 @@
+<html>
+<title>UTS_SITI_FADHILAH(SKA-15.A.2)</title>
+<form action="proses.php" method="get">
+<body>
+<div id="header"><h2>Pendaftaran Pelanggan Baru</h2></div>
+<pre>
+<tr>
+<td id="P"><b>Data Pribadi</b></td><br>
+Nama Lengkap    : <input type="text" name="nama" /><br>
+Jenis Kelamin   : <input tytpe="text" name="jk" /><br>
+E-mail          : <input tytpe="text" name="email" /><br><br>
+
+<td id="P"><b>Data Alamat Pengiriman</b></td><br>
+Nama Negara 	: <input type="text" name="negara" /><br>
+Kota			: <input type="text" name="kota" /><br>
+Alamat			: <input type="text" name="alamat" /><br>
+Kode Pos		: <input type="text" name="kodepos" /><br><br>
+
+<td id="P"><b>Data Nomor Kontak</b></td><br>
+Nomor Telpon	: <input type="text" name="telpon" /><br>
+Nomor Handphone	: <input type="text" name="hp" /><br>
+Nomor Faksimili	: <input type="text" name="faks" /><br><br>
+
+</pre>
+		<input type="submit" value="Submit">
+		<input type="reset" value="Reset">
+
+</form>
+</body>
+</html>
---
+++ proses.php
@@ -0,0 +1,21 @@
+<?php
+	echo $_GET['nama'];
+	echo "<br/>";
+	echo $_GET['jk'];
+	echo "<br/>";
+	echo $_GET['email'];
+	echo "<br/>";
+	echo $_GET['negara'];
+	echo "<br/>";
+	echo $_GET['kota'];
+	echo "<br/>";
+	echo $_GET['alamat'];
+	echo "<br/>";
+	echo $_GET['kodepos'];
+	echo "<br/>";
+	echo $_GET['telpon'];
+	echo "<br/>";
+	echo $_GET['hp'];
+	echo "<br/>";
+	echo $_GET['faks'];
+?>
